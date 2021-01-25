ce4a123609b738a16c20b021ef352eea507b1720
Øyvind Rønne <oyron@equinor.com>
2021-01-25 16:01:28 +0100

    Update cors settings

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const path = require('path');
 function app(authModule) {
     const api = require('./routes/api')(authModule);
     expressApp.use(bodyParser.json());
-    expressApp.use(cors({origin: ['https://editor.swagger.io', 'http://editor.swagger.io', 'http://localhost:8080', 'http://localhost:3100', 'https://gui-swagger-editor-single.playground.radix.equinor.com/']}));
+    expressApp.use(cors({origin: ['http://gui-swagger-editor-single.playground.radix.equinor.com', 'https://gui-swagger-editor-single.playground.radix.equinor.com', 'http://localhost:8080']}));
     expressApp.use(express.static(path.join(__dirname, 'static')));
     expressApp.use('/api', api);
     return expressApp;
